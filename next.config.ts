75516289932133b39fc84c9597c41f86cee2f6f8
FarmzDev <153562375+FarmzDev@users.noreply.github.com>
2025-01-02 00:29:34 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,38 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'static.qobuz.com',
-        port: '',
-        pathname: '**',
-        search: '',
-      },
-    ],
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'static.qobuz.com',
+                port: '',
+                pathname: '**',
+                search: '',
+            },
         ],
-      },
-    ];
-  },
+    },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "*",
+                    },
+                    {
+                        key: 'Cross-Origin-Opener-Policy',
+                        value: 'same-origin',
+                    },
+                    {
+                        key: 'Cross-Origin-Embedder-Policy',
+                        value: 'require-corp',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
